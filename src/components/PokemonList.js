5bfcf4ee5cbf8bb82f6e9f04b547baa47436e4dd
gandhia <animesh.gandhi@earlywarning.com>
2023-02-02 16:51:21 -0700

    Added logic to fetch names from txt file

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import HekkinDiv from './HekkinDiv';
 import '../css/pokemonList.css'
+import raw from '../resources/names.txt'
 
 export default class PokemonList extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            pokemonList: []
+        }
+    }
+    
+    componentDidMount(){
+        fetch(raw)
+        .then(r => r.text())
+        .then(text => {
+            let temp = []
+            let names = text.split("\n")
+
+            for(let i = 0; i < names.length; i++){
+                let url = "https://pokeapi.co/api/v2/pokemon-form/" + names[i].toLowerCase()
+                temp.push(<a href={url}><HekkinDiv /></a>)
+            }
+            return temp
+        })
+        .then((temp) => {
+            this.setState({
+                pokemonList:temp
+            })
+        })
+    }
 
     render(){
-        let arr = [];
-        let names = ['eevee', 'meowth', 'flareon', 'jolteon']
-        for(let i = 0; i < names.length; i++){
-            let url = "https://pokeapi.co/api/v2/pokemon-form/" + names[i];
-            arr.push(<a href={url}><HekkinDiv /></a>);
-        }
-
         return(
             <div className='list'>
-                {arr}
+                    {this.state.pokemonList}
             </div>
         )
     }
